04fb73542f8521bbd06ee0c9c1b4ab684478f276
franTarkenton <kevin.netherton@gov.bc.ca>
2022-01-21 12:48:09 -0800

    the minor changes that we made to files during training delivery

--- python/Lesson1-Secrets/constants.py
+++ python/Lesson1-Secrets/constants.py
@@ -3,20 +3,31 @@ import dotenv
 import sys
 
 # looking for a .env file in the directory that this module is located
-envFileName = '.env'
-envPath = os.path.join(os.path.dirname(__file__), envFileName)
+envFileName = '.env-tj'
+secretsDir = r'/mnt/c/junk/secrets'
+currentDirectory = os.path.dirname(__file__)
+envPath = os.path.join(secretsDir, envFileName)
+print(envPath)
 
 # if the .env file is found then load it
 if os.path.exists(envPath):
+    print('loading env vars')
     dotenv.load_dotenv(envPath)
 
 BILLS_PASSWORD = os.environ['BILLS_PASSWORD']
 API_KEY = os.environ['API_KEY']
 
+
+
 # optional params: if they are defined in env vars, the are populated into
 # variables for this module.
-optionals = ['TEST_OBJ_NAME', "SUPERSECRET", 'CRAZYENV', 'ETC']
+optionals = ['TEST_OBJ_NAME', "SUPERSECRET", 'CRAZYENV', 'ETC', 'DEMO']
 module = sys.modules[__name__]
 for optional in optionals:
     if optional in os.environ:
         setattr(module, optional, os.environ[optional])
+
+
+if __name__ == '__main__':
+    print(f"the demo var is {os.environ['DEMO']}")
+
--- python/Lesson1-Secrets/s3_secrets_usingconst.py
+++ python/Lesson1-Secrets/s3_secrets_usingconst.py
@@ -1,4 +1,7 @@
 import constants
-
+from constants import BILLS_PASSWORD
 # you can now access any of your constants from constants module
 print(f"bills password is {constants.BILLS_PASSWORD}")
+print(f"DEMO env var is {constants.DEMO}")
+
+print(f"bills password is {BILLS_PASSWORD}")
